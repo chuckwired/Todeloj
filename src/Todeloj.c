ea042a9976b33ab713d76ab8a1d40c6734daae85
Chuck Rice <charles.rice92@gmail.com>
2015-05-31 14:59:07 +0100

    basic clock

--- src/Todeloj.c
+++ src/Todeloj.c
@@ -1,20 +1,39 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *time_layer;
+
+static void update_time() {
+	time_t temp = time(NULL);
+	struct tm *tick_time = localtime(&temp);
+	
+	//Do the update
+	static char buffer[] = "00:00";
+	if(clock_is_24h_style() == true){
+		strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+	} else {
+		strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+	} text_layer_set_text(time_layer, buffer);
+}
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  //Setuip + styling
+  time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  //Add
+  layer_add_child(window_layer, text_layer_get_layer(time_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(time_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	update_time();
 }
 
 static void init(void) {
@@ -23,7 +42,9 @@ static void init(void) {
     .load = window_load,
     .unload = window_unload,
   });
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   window_stack_push(window, true);
+  update_time();
 }
 
 static void deinit(void) {
