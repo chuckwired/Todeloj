116203c9941c6ad734f22a388cc15ecb09d9306c
Chuck Rice <charles.rice92@gmail.com>
2015-06-28 17:39:32 +0100

    day and date plus minor style update

--- src/Todeloj.c
+++ src/Todeloj.c
@@ -2,6 +2,7 @@
 
 static Window *window;
 static TextLayer *time_layer, *hour_layer, *minute_layer, *date_layer, *day_layer;
+static InverterLayer *inverter_layer;
 
 static void update_time() {
 	time_t temp = time(NULL);
@@ -10,19 +11,23 @@ static void update_time() {
 	//Update time buffers
 	static char timeBuffer[] = "00:00";
 	if(clock_is_24h_style() == true){
-		strftime(timeBuffer, sizeof("00:00"), "%H:%M", tick_time);
+		strftime(timeBuffer, sizeof(timeBuffer), "%H:%M", tick_time);
 	} else {
-		 strftime(timeBuffer, sizeof("00:00"), "%I:%M", tick_time);
+		 strftime(timeBuffer, sizeof(timeBuffer), "%I:%M", tick_time);
 	} 
-	
-	//Set the values
+	//Handle leading 0's
 	if (timeBuffer[0] == '0') {timeBuffer[0] = ' '; }
 	text_layer_set_text(time_layer, timeBuffer);
 	
 	//Dates
-	static char dateBuffer[] = "JAN 01";
-	strftime(dateBuffer, sizeof("JAN 01"), "%B %I", tick_time);
+	static char dateBuffer[] = "DECEMBER 01";
+	strftime(dateBuffer, sizeof(dateBuffer), "%B %e", tick_time);
 	text_layer_set_text(date_layer, dateBuffer);
+	
+	//Day
+	static char dayBuffer[] = "SuN";
+	strftime(dayBuffer, sizeof(dayBuffer), "%a", tick_time);
+	text_layer_set_text(day_layer, dayBuffer);
 }
 
 static void window_load(Window *window) {
@@ -30,21 +35,33 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   //Time layer
-  time_layer = text_layer_create(GRect(0, 55, 144, 50));
-  text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
+  time_layer = text_layer_create(GRect(10, 112, 144, 50));
+  text_layer_set_text_alignment(time_layer, GTextAlignmentLeft);
   text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
   layer_add_child(window_layer, text_layer_get_layer(time_layer));
   
   //Date layer
-  date_layer = text_layer_create(GRect(0, 120, 144, 50));
-  text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
-  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  date_layer = text_layer_create(GRect(10, 5, 144, 50));
+  text_layer_set_text_alignment(date_layer, GTextAlignmentLeft);
+  text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
   layer_add_child(window_layer, text_layer_get_layer(date_layer));
+  
+  //Day layer
+  day_layer = text_layer_create(GRect(110, 112, 25, 25));
+  text_layer_set_text_alignment(day_layer, GTextAlignmentRight);
+  text_layer_set_font(day_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  layer_add_child(window_layer, text_layer_get_layer(day_layer));
+  
+  //Inverter layer
+  inverter_layer = inverter_layer_create(GRect(0, 0, 144, 80));
+  layer_add_child(window_layer, inverter_layer_get_layer(inverter_layer));
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(time_layer);
   text_layer_destroy(date_layer);
+  text_layer_destroy(day_layer);
+  inverter_layer_destroy(inverter_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -59,7 +76,6 @@ static void init(void) {
   });
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   window_stack_push(window, true);
-  update_time();
 }
 
 static void deinit(void) {
